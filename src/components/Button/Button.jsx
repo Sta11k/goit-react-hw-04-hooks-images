e8e13e65c708051202e03cf4bedcb5d0b885c2c1
Sta11k <Alexandiel1984@gmail.com>
2021-11-08 15:07:20 +0200

    button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,12 +1,7 @@
-import { Component } from 'react';
-// import { Component } from 'react';
-
-export default class Button extends Component {
-  render() {
-    return (
-      <button type="button" className="Button" onClick={this.props.loadMorer}>
-        {this.props.text}
-      </button>
-    );
-  }
+export default function Button({ text, loadMorer }) {
+  return (
+    <button type="button" className="Button" onClick={loadMorer}>
+      {text}
+    </button>
+  );
 }
