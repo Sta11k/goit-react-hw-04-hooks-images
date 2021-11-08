8000db46886fd14627a194bebb65b80efc4c36bb
Sta11k <Alexandiel1984@gmail.com>
2021-11-08 16:54:09 +0200

    startSetApp

--- src/services/apiSearchImg.jsx
+++ src/services/apiSearchImg.jsx
@@ -6,8 +6,8 @@ export class PixabayFetchFunc {
     this.API_KEY = API_KEY;
     this._searchQuery = '';
     this._searchPage = 1;
+    this._onLoader = false;
   }
-
   get searchQuery() {
     return this._searchQuery;
   }
@@ -26,6 +26,12 @@ export class PixabayFetchFunc {
   set searchPage(value) {
     return (this._searchPage += value);
   }
+  // get onLoader() {
+  //   return this._onLoader;
+  // }
+  // set onLoader(value) {
+  //   return this._onLoader !== value;
+  // }
 
   async getImages() {
     axios.defaults.baseURL = this.BASE_URL;
@@ -47,3 +53,22 @@ export class PixabayFetchFunc {
     }
   }
 }
+
+// import axios from 'axios';
+
+// const PixabayFetchFunc = async ({
+//   BASE_URL,
+//   API_KEY,
+//   searchPage = 1,
+//   searchQuery,
+// }) => {
+//   axios.defaults.baseURL = BASE_URL;
+//   let url = `?q=${searchQuery}&page=${searchPage}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
+
+//   const result = await axios.get(url);
+//   const data = await result.data.hits;
+
+//   return data;
+// };
+
+// export default PixabayFetchFunc;
